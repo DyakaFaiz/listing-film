a67b9bdd917ef151e2fd93259c0fae253a43dcc2
DyakaFaiz <dyakafaiz123@gmail.com>
2024-01-24 00:50:28 +0700

    upload file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { getMovieList, searchMovie } from './api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+const [popularMovies, setPopularMovies] = useState([])
+
+useEffect(()=> {
+  getMovieList().then((results)=>{
+    setPopularMovies(results)
+  });
+}, [])
+
+const imageUrl = process.env.REACT_APP_BASEIMGURL
+
+const PopularMovieList = ()=>{
+  return popularMovies.map((movie, i )=>{
+    console.log(movie)
+    return(
+      <div className="wrapper" key={i}> 
+        <img className="movie-image" src={`${imageUrl}${movie.poster_path}`} alt='img-movie' />
+        <div className="movie-title">{movie.title}</div>
+        <div className="movie-date">Tanggal Rilis : {movie.release_date}</div>
+        <div className="movie-rating">Rating : {movie.vote_average} ⭐</div>
+      </div>
+    )
+  })
 }
 
-export default App;
+const search = async(n) =>{
+  if(n.length >3){
+    const query = await searchMovie(n)
+    setPopularMovies(query.results)
+}
+}
+
+return (
+<div className="App">
+  <header className="App-header">
+    <input type="text" placeholder='Cari Film...' className='movie-search' onChange={({target})=> search(target.value)}
+    />
+    <div className="container">
+      <PopularMovieList />
+    </div>
+  </header>
+</div>
+);
+}
---
+++ src/api.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const baseUrl = process.env.REACT_APP_BASEURL
+const apiKey = process.env.REACT_APP_APIKEY
+
+export const getMovieList = async() =>{
+const movie = await axios.get(`${baseUrl}/movie/popular?page=1&api_key=${apiKey}`)
+return(movie.data.results)
+}
+
+export const searchMovie = async(n)=>{
+    const search = await axios.get(`${baseUrl}/search/movie?query=${n}&page=1&api_key=${apiKey}`)
+    return search.data
+}
